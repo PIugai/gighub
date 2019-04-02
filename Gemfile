00c05d576a3e2e6a9e9f44fc5dc5587eb9820d6a
Pavel Iugai <pavel.iugai@outlook.com>
2019-04-02 14:52:15 +0800

    Add dotenv - Protect my secret data in .env file

--- Gemfile
+++ Gemfile
@@ -13,6 +13,9 @@ gem 'font-awesome-sass', '~> 5.6.1'
 gem 'sassc-rails'
 gem 'simple_form'
 gem 'uglifier'
+
+gem 'rest-client'
+gem 'dotenv-rails', groups: [:development, :test]
 # gem 'webpacker'
 
 group :development do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_02_024637) do
+ActiveRecord::Schema.define(version: 2019_04_02_063434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2019_04_02_024637) do
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "open_id"
   end
 
   add_foreign_key "bands", "users"
