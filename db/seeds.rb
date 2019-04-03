7b1a46ce36b7a6f3e6a3c4bb4be11ff177a87f6d
Kelvin Lee <kelvin_lee@leaderradio.cn>
2019-04-03 11:05:30 +0800

    finished better seed and adding style column to band model

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ require 'faker'
 require 'date'
 
 # Create 50 users in order to create 50 bands, since each band must have a user (band has user as a foreign ID)
-50.times do
+20.times do
   User.create(name: Faker::Name.name, email: Faker::Internet.email, photo: "http://loremflickr.com/200/200/faceshot")
 end
 
@@ -16,17 +16,20 @@ end
 # link
 # user_id
 
+counter = 0
 User.all.each do |user|
   name1 = Faker::Kpop.i_groups
   name2 = Faker::Music::RockBand.name
   bandname = [name1, name2].sample
-  photoURL = "http://loremflickr.com/300/150/rockband"
+  photoURL = "http://loremflickr.com/300/#{150+counter}/rockband"
+  counter += 1
   city = %w(Shanghai Beijing Shenzhen Chengdu Hangzhou Nanjing Chongqing).sample
   randomcost = [100, 120, 150, 199, 250, 280, 500, 800, 1500, 2000, 10000].sample
   lorem = Faker::Lorem.paragraph(rand(2..4))
   randomlinks = "https://v.youku.com/v_show/id_XODMzNDcwMzM2.html"
+  randomstyle = %w(Dubstep Folk Pop Opera Rock Country Rap Trance Latin Gospel J-pop Ska Reggae Bhangra Trip-hop).sample
   # every band has the same video for now... unless we have time to seed more variety
-  new_band = Band.create(name: bandname, photo: photoURL, location: city, price: randomcost, description: lorem, link: randomlinks, user: user)
+  new_band = Band.create(name: bandname, photo: photoURL, location: city, price: randomcost, description: lorem, link: randomlinks, user: user, style: randomstyle)
 end
 
 20.times do
