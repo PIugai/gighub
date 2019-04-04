e2f445a3d0591b645cf7cda7c5692c3f2bcbdddf
Erin Chuah <chuah.erin@gmail.com>
2019-04-04 14:11:18 +0800

    unnested routes

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -1,6 +1,25 @@
 class Api::V1::BookingsController < Api::V1::BaseController
-  def index
-    @user = User.find(params[:user_id])
-    @bookings = @user.bookings
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      status: :created
+    elese
+      render_error
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date, :user_id, :band_id)
+  end
+
+  def render_error
+    render json: { errors: @booking.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,9 @@ Rails.application.routes.draw do
   # root to: 'pages#home'
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: [:show, :new, :create] do
-        resources :bands, only: [:new, :create]
-        # resources :bookings, only: [:index]
-      end
-      resources :bands, only: [:index, :show]
+      resources :users, only: [:show, :new, :create]
+      resources :bands, only: [:index, :show, :new, :create]
+      resources :bookings, only: [:new, :create]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
